ad3a9ddd287144b4985d242b107b43790821d7e3
imvne <imanebeaufils19@gmail.com>
2024-04-24 18:17:30 +0200

    fonction résolution

--- eau01.js
+++ eau01.js
@@ -1,26 +1,30 @@
 // Combinaison de deux nombres
 
 // Solving
+function resolution(){
+	let list = [];
 
-let list = [];
-
-for (let i = 0; i < 99; i++) {
-	let j = i + 1;
-	for (; j <= 99; j++) {
-		let firstNumber = i.toString().padStart(2, 0);
-		let secondNumber = j.toString().padStart(2, 0);
-		if (i !== 98) {
-			list += firstNumber + " " + secondNumber + ", ";
+	for (let i = 0; i < 99; i++) {
+		let j = i + 1;
+		for (; j <= 99; j++) {
+			let firstNumber = i.toString().padStart(2, 0);
+			let secondNumber = j.toString().padStart(2, 0);
+			if (i !== 98) {
+				list += firstNumber + " " + secondNumber + ", ";
+			}
+			else {
+				list += firstNumber + " " + secondNumber;
+			}
+			
 		}
-		else {
-			list += firstNumber + " " + secondNumber;
-		}
-		
 	}
+	return list
 }
 
 
+
+
 // Print
-console.log(list);
+console.log(resolution());
 
 // résolu en 1h
