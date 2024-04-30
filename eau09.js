d00486588507386780797988351babc8afec17d8
imvne <imanebeaufils19@gmail.com>
2024-04-30 21:14:49 +0200

    entre min et max

---
+++ eau09.js
@@ -0,0 +1,93 @@
+// Entre min et max
+
+// Useful functions
+function slice(arguments, firstIndex, endIndex){
+	if (Array.isArray(arguments)){
+		let argumentsSliced = [];
+		for (let i = firstIndex ; i <= endIndex ; i++){
+			argumentsSliced.push(arguments[i])
+		}
+		return argumentsSliced
+	}
+	else {
+		let argumentsSliced = "";
+		for (let i = firstIndex ; i <= endIndex ; i++){
+			argumentsSliced += arguments[i]
+		}
+		return argumentsSliced
+	}
+}
+
+function getMinMax(arg1, arg2){
+	let minMaxArr = [];
+	if (arg1 < arg2){
+		let min = arg1;
+		let max = arg2;
+		minMaxArr.push(min, max)
+	}
+	else {
+		let min = arg2;
+		let max = arg1;
+		minMaxArr.push(min, max)
+	}
+	return minMaxArr
+}
+
+function getBetweenMinMax(arg1, arg2){
+	const minMaxArr = getMinMax(arg1, arg2);
+	const min = parseInt(minMaxArr[0]);
+	const max = parseInt(minMaxArr[1]);
+	let betweenMinMaxArr = [];
+	for (let i = 0; i < max-min; i++){
+		betweenMinMaxArr.push(min+i)
+	}
+	return betweenMinMaxArr
+}
+
+// Error management
+
+function checkArguments(arg){
+	if (arg.length === 2) {
+		return true;
+	}
+	return false
+}
+
+function checkArgumentType(arg){
+	for (a of arg){
+		if (/^\d+$/.test(a)){
+			return true;
+		}
+	}
+	return false
+}
+
+
+// Parsing
+
+function getArguments(){
+	const argSliced = slice(process.argv, 2, process.argv.length-1) 
+	return argSliced
+	
+}
+
+
+// Solving
+
+function betweenMinMax(){
+	const argument = getArguments();
+	if (!checkArguments(argument)){
+		return 'erreur : insérez deux arguments'
+	}
+	else if (!checkArgumentType(argument)){
+		return "erreur : n'insérez que des nombres"
+	}
+	else {
+		return getBetweenMinMax(argument[0], argument[1])
+	}
+}
+
+
+// Print
+
+console.log(betweenMinMax())
