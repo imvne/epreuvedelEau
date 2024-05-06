fe2cf29bdb83aa5187a2d01805e66ca8c9f8a324
imvne <imanebeaufils19@gmail.com>
2024-05-06 20:06:15 +0200

    noms fonctions variables

--- eau00.js
+++ eau00.js
@@ -20,11 +20,10 @@ function isSortedNumber (number) {
 }
 
 // Solving
-function getResolution(){
-	let i = 0;
-	let combinaisonList = "";
+function threeDigitsCombination(){
+	let combinationList = "";
 	
-	for (; i < 999; i++) {
+	for (let i = 0 ; i < 999 ; i++) {
 		let number = i.toString().padStart(3, 0);			
 		if (isRepeatedNumber(number)) {
 			continue;
@@ -32,18 +31,15 @@ function getResolution(){
 		else if (!isSortedNumber(number)) {
 			continue;
 		}
-		let combinaison = number.padStart(3, '0')
-		combinaisonList += number + " ";
+		combinationList += number + " ";
 	}
-	return combinaisonList;
+	return combinationList;
 }
 
 
 // Print
-console.log(getResolution());
+console.log(threeDigitsCombination());
 
 
-// je défini number en tant que string pour qu'il soit considéré comme un tableau et que ce soit simple d'atteindre chaque chiffre d'un nombre
+// je défini number en tant que string pour qu'il soit considéré comme un tableau et que ce soit simple d'atteindre chaque unité d'un nombre
 // je défini le format de number à trois unités, si on est rendu à 12, 012 est pris en compte pour pouvoir vérifier les deux fonctions
-// j'ajoute ces zéros manquant sur mon affichage final
-// résolu en 1h37
