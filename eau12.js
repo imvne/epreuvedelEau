9a41c7fbd52f18a443587d36d3382837a293f87a
imvne <imanebeaufils19@gmail.com>
2024-05-06 21:51:42 +0200

    array

--- eau12.js
+++ eau12.js
@@ -28,19 +28,19 @@ function charToNumber(arg){
 	return numberArr
 }
 
-function bubblesort(arr){
-	for (let i = 0 ; i < arr.length-1 ; i++){
+function bubblesort(array){
+	for (let i = 0 ; i < array.length-1 ; i++){
 		
-		for (let j = 0 ; j < arr.length-1 ; j++){
-			if (arr[j] > arr[j+1]){
-				let temp = arr[j]
-				arr[j] = arr[j+1]
-				arr[j+1] = temp
+		for (let j = 0 ; j < array.length-1 ; j++){
+			if (array[j] > array[j+1]){
+				let temp = array[j]
+				array[j] = array[j+1]
+				array[j+1] = temp
 			}
 		}
 		
 	}
-	return arr.join(' ')
+	return array.join(' ')
 }
 
 // Error management
